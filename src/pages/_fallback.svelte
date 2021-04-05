2f9f8fc11497eeb2b7362bde4844bfd7f786fd6f
Bluerberry <boazkreulen.main@gmail.com>
2021-04-06 01:25:44 +0200

    Ported legacy code, we can get going now

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -1,25 +1,20 @@
 <script>
-    import { url } from '@roxi/routify'
+	import { url } from '@roxi/routify'
 </script>
 
 <style>
-  .huge {
-    font-size: 12rem;
-  }
-  
-  .e404 {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-  }
+	h1, p {
+		margin: 0px;
+
+		text-align: center;
+	}
+
+	h1 {
+		font-size: 12rem;
+	}
 </style>
 
-<div class="e404">
-  <div class="huge">404</div>
-  <div class="big">Page not found. 
-  <!-- link to the parent folder of _fallback.svelte -->
-  <a href={$url('../')}>Go back</a>
-  </div>
+<div>
+	<h1> 404 </h1>
+	<p> Page not found. <a href={$url('../')}>Go back</a></p>
 </div>
